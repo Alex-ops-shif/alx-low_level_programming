2f16c9bd7f093e2319ab1246d468d4cb3919ab68
Alex-ops-shif <alexchegemaina@gmail.com>
2022-12-13 14:55:14 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * return :always returs 0 (succes)
+ * Return : Always returs 0 (succes)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
