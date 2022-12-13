77f831cb73f9bfc298f29950d198a3d2aabab469
Alex-ops-shif <alexchegemaina@gmail.com>
2022-12-13 15:21:09 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+	printf("size of char : %d byte(s)\n", sizeof(char));
+	pritf("size of int : %d byte(s)\n", sizeof(int));
+	printf("size of long int : %d byte(s)\n", sizeof(long int));
+	printf("size of long long int : %d byte(s)\n", sizeof(long long int));
+	printf("size of float : %d byte(s)\n", sizeof(float));
+	return (0);
+}
